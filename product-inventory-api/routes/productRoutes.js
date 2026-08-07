4bf5d560ec461c66c793d3f966f0ce9f1687b0cc
Issachar01 <issachardereje010@gmail.com>
2026-08-07 08:37:27 -0900

    Second Commit: Adding Filtering Features

--- product-inventory-api/routes/productRoutes.js
+++ product-inventory-api/routes/productRoutes.js
@@ -1,17 +1,27 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 const {
   getAllProducts,
   getProductByID,
   addProduct,
   updateProduct,
-  deleteProduct
-} = require('../controllers/productControllers');
+  deleteProduct,
+  getProductsByCategory,
+  getProductsUnderPrice,
+  getLowStockProducts,
+  getTotalInventoryValue,
+  validateProduct
+} = require('../controllers/productController');
+
+router.get('/category/:category', getProductsByCategory);
+router.get('/price/:price', getProductsUnderPrice);
+router.get('/low-stock', getLowStockProducts);
+router.get('/value', getTotalInventoryValue);
 
 router.get('/', getAllProducts);
 router.get('/:id', getProductByID);
-router.post('/', addProduct);
-router.put('/:id', updateProduct);
+router.post('/', validateProduct, addProduct);
+router.put('/:id', validateProduct, updateProduct);
 router.delete('/:id', deleteProduct);
 
 module.exports = router;
